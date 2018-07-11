9821f7d23c08ee5b0b0251830d440823cece0d98
Emilio Cunha <36512099+cunhaemilio@users.noreply.github.com>
2018-07-11 11:10:39 -0300

    Update env.rb

--- features/support/env.rb
+++ features/support/env.rb
@@ -8,4 +8,6 @@ Capybara.configure do |config|
     config.default_driver = :selenium
     config.app_host = 'https://automacaocombatista.herokuapp.com/'
     config.default_max_wait_time = 15
+    
+    desired_capabilities: { accept_insecure_certs: true }
 end
