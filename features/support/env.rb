eddfc04f2479aac09b8e3566479d362328478927
Emilio Cunha <36512099+cunhaemilio@users.noreply.github.com>
2018-07-11 12:07:20 -0300

    Update env.rb

--- features/support/env.rb
+++ features/support/env.rb
@@ -16,3 +16,30 @@ Capybara.configure do |config|
   config.default_driver = :insecure_selenium
 end
 
+
+###
+
+$env = ENV['BROWSER']
+$headless = ENV['HEADLESS']
+
+if $headless
+    Capybara.register_driver :selenium do |app|
+    Capybara::Poltergeist::Driver.new(app, js_errors: false)
+end
+end
+Capybara.configure do |c|
+
+if $env == 'firefox'
+    c.default_driver = :selenium
+elsif $env == 'chrome'
+    c.default_driver = :selenium_chrome
+elsif $env == 'chrome_headless'
+    c.default_driver = :selenium_chrome_headless
+
+end
+
+Capybara.default_max_wait_time = 20
+    
+end
+
+
